36e5d445caba2b485ab24b1dc4a452c618faa554
navegantemx <raulvelazquez437@gmail.com>
2020-09-11 05:17:09 +0000

    056 deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutique_rvg.wsgi:application
--- boutique_rvg/settings.py
+++ boutique_rvg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'q2*%h^f71bf^o72gf%8k92t(mz&zf_ucy3r_rirb327w!y+^q5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['navegantemx-boutique-rvg.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'boutique_rvg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

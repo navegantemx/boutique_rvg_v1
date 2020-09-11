e89f2c6ab79f06ed7057f52b054d59b63389011a
navegantemx <raulvelazquez437@gmail.com>
2020-09-11 05:41:22 +0000

    Removed secret key and set debug

--- boutique_rvg/settings.py
+++ boutique_rvg/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q2*%h^f71bf^o72gf%8k92t(mz&zf_ucy3r_rirb327w!y+^q5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['navegantemx-boutique-rvg.herokuapp.com', 'localhost']
 
